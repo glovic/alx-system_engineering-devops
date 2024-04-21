bbf2d5e871e3760e3fa59e9b4706f7f17a44e265
glovic <xtineviktor@gmail.com>
2024-04-22 00:55:42 +0100

    creating puppet file(fixing typo)

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Create a file in /tmp
 
-file {'school':
-ensure => 'present',
-path => '/tmp/school',
-mode => '0744',
-owner =>'www-data',
-group => 'www-data',
-content = 'I love Puppet',
+file {'/tmp/school':
+ensure  => 'file',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
