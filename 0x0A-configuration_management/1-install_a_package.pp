f830e3a3ee22f6605d4408d50e2645ad2c010cbf
glovic <xtineviktor@gmail.com>
2024-04-22 01:30:53 +0100

    installing flask using puppet (requirment update)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,12 @@
 #Manifest install flask form pip3
 
-package {'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
 }
 
-#package {'werkzeug':
-#ensure   => '2.1.1',
-#provider => 'pip3',
-#} 
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
