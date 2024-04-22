9cf05186ff2f4b18fbbf95f8bdc1b6796d9876b2
glovic <xtineviktor@gmail.com>
2024-04-22 01:46:53 +0100

    Updated Readme.md

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
 #Manifest install flask form pip3
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
 
