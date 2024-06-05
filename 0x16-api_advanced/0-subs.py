1264ce4f8639cab16736f0af6ed63dcf1add4b6a
glovic <xtineviktor@gmail.com>
2024-06-05 15:32:56 +0100

    queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-
-"""
-  A function to query subscribers on a given Reddit subreddit.
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+        "User-Agent": "MyRedditClient/1.0 (by /u/victorglo)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
-
+    results = response.json().get("data")
+    return results.get("subscribers")
